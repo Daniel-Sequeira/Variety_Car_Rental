65dcfb31767323d6a81384aa34f89815a38c4a7f
Fabian Arredondo <fabianarredondo365@gmail.com>
2024-03-04 22:20:25 -0600

    Actualizo

--- VarietyCarRental/obj/Debug/net8.0/VarietyCarRental.AssemblyInfo.cs
+++ VarietyCarRental/obj/Debug/net8.0/VarietyCarRental.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VarietyCarRental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ac73fe88d58616485049e42a51de738fcacccbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a71d4b0591df695ba25bfd8f6935530323e079be")]
 [assembly: System.Reflection.AssemblyProductAttribute("VarietyCarRental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VarietyCarRental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

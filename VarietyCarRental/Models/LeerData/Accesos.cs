658a321ba49d04ab826d865dd3fec8dcb8cbf995
Fabian Arredondo <fabianarredondo365@gmail.com>
2024-03-15 11:38:47 -0600

    Subo clases actualizadas

--- VarietyCarRental/Models/LeerData/Accesos.cs
+++ VarietyCarRental/Models/LeerData/Accesos.cs
@@ -2,16 +2,7 @@
 {
     public class Accesos
     {
-<<<<<<< HEAD
         public int id_accesos {  get; set; }
         public string nombre_acceso { get; set; }
-=======
-
-        public int IdAcceso {  get; set; }
-        public string NombreAcceso { get; set; }
-        public int IdPermiso { get; set; }
-
-        public Usuarios Usuarios { get; set; }
->>>>>>> af881ef1ab70bb9fba0c5872c107d8ca59ddd11b
     }
 }

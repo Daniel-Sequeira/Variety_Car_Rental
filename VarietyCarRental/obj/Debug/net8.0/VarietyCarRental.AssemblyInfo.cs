5c4de0daff024b2e84a2f3be873a77e162b89aef
Daniel Sequeira <danielsequeira22@gmail.com>
2024-03-27 22:25:13 -0600

    actualizacion de cambios del pc remoto

--- VarietyCarRental/obj/Debug/net8.0/VarietyCarRental.AssemblyInfo.cs
+++ VarietyCarRental/obj/Debug/net8.0/VarietyCarRental.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VarietyCarRental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a260c6e4a94fd63d9494afe1649d3649a2529dc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a475113363303a6d00d3866b967557a2bdcf5af3")]
 [assembly: System.Reflection.AssemblyProductAttribute("VarietyCarRental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VarietyCarRental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
